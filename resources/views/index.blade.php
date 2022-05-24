0ba694e1b31f2b8ecc15f279d1ad6a98cbfeaecf
Ankur Kumar <ankurkumar.9266@gmail.com>
2022-05-25 01:13:37 +0530

    archive heading added.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,11 @@
     @includeUnless(request()->has('month') && request()->has('year'), 'partials.featured_post')
     <div class="row g-5">
         <div class="col-md-8">
+            @if (request()->has('month') && request()->has('year'))
+                <h3 class="pb-4 fst-italic border-bottom">
+                    Archive : {{ ucwords(request()->month . ' ' . request()->year) }}
+                </h3>
+            @endif
             <div class="d-flex flex-wrap justify-content-between">
                 @include('partials.post_card')
             </div>
